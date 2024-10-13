a6c7b382360718982ae8b20d03d9255faf70541d
Pavel Ignatenko <pavel.ignatenko1996@gmail.com>
2024-10-13 14:11:04 +0800

    update local storage functionality

--- src/helpers/handleLocalStorage.ts
+++ src/helpers/handleLocalStorage.ts
@@ -1,2 +1,9 @@
-export const getCenterFromlStorage = (): [number, number] =>
-  localStorage.getItem('centerCoords') ? JSON.parse(localStorage.getItem('centerCoords')!) : [34.057124, -118.388622];
+export const getCenterFromStorage = (): [number, number] => {
+  const stored = localStorage.getItem('centerCoords');
+  return stored ? JSON.parse(stored!) : [34.057124, -118.388622];
+};
+
+export const getZoomFromStorage = (): number => {
+  const stored = localStorage.getItem('zoom');
+  return stored ? JSON.parse(stored!) : 12;
+}
