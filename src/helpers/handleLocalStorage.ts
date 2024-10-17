5363d70d220ae94eb26426386bc3ca59b1442157
Pavel Ignatenko <pavel.ignatenko1996@gmail.com>
2024-10-17 18:10:07 +0800

    update default center coords and zoom value

--- src/helpers/handleLocalStorage.ts
+++ src/helpers/handleLocalStorage.ts
@@ -1,9 +1,9 @@
 export const getCenterFromStorage = (): [number, number] => {
   const stored = localStorage.getItem('centerCoords');
-  return stored ? JSON.parse(stored!) : [34.057124, -118.388622];
+  return stored ? JSON.parse(stored!) : [40.281417784856544, 9.625139236450195];
 };
 
 export const getZoomFromStorage = (): number => {
   const stored = localStorage.getItem('zoom');
-  return stored ? JSON.parse(stored!) : 12;
+  return stored ? JSON.parse(stored!) : 16;
 }
