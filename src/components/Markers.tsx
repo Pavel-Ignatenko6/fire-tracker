6f73d3bf6eb1d3e51a00aef5dced284ec2f2eb9a
Pavel Ignatenko <pavel.ignatenko1996@gmail.com>
2024-11-17 16:18:26 +0800

    create popup and sideBar info logic

--- src/components/Markers.tsx
+++ src/components/Markers.tsx
@@ -1,18 +1,51 @@
+import { useRef, useEffect } from 'react';
 import { objectsUrl } from '../App';
-import { Marker } from 'react-leaflet';
+import { Marker, Popup, useMap } from 'react-leaflet';
 import { useFetchMapData } from '../hooks/useFetchMapData';
+// redux
+import { useDispatch, useSelector } from 'react-redux';
+import { setSidebarObject } from '../state/sidebarObjectState';
+import { popupStateValue } from '../state/popupStateSlice';
 
 export const Markers = () => {
+  const map = useMap();
+  const mapObjects = useFetchMapData(objectsUrl);
+
+  const dispatch = useDispatch();
+
+  const popupRef = useRef(null);
+  const popupState = useSelector(popupStateValue);
+
+  // close popup when popupState is false
+  useEffect(() => {
+    if (!popupState) {
+      map.closePopup();
+    }
+  });
+  
   return (
     <>
-      {useFetchMapData(objectsUrl).map(
+      {mapObjects.map(
         singleMapObject =>
           singleMapObject.marker !== 'resourceMarker' && (
-            <Marker
+            <div
+              className='marker-container h-2.5 w-2.5'
               key={singleMapObject.id}
-              position={singleMapObject.coords}
-              title={singleMapObject.title}
-            ></Marker>
+            >
+              <Marker
+                key={singleMapObject.id}
+                position={singleMapObject.coords}
+                title={singleMapObject.title}
+                // get mapObject data for sidebar on click v 
+                eventHandlers={{
+                  click: () => dispatch(setSidebarObject(singleMapObject)),
+                }}
+              >
+                <Popup ref={popupRef}>
+                  <p className='first-letter:capitalize text-base'>{singleMapObject.title}</p>
+                </Popup>
+              </Marker>
+            </div>
           )
       )}
     </>
