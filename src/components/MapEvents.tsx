ba42ead245b9e24133fed38ac046c42203125165
Pavel Ignatenko <pavel.ignatenko1996@gmail.com>
2024-11-17 16:15:53 +0800

    add popup events

--- src/components/MapEvents.tsx
+++ src/components/MapEvents.tsx
@@ -1,11 +1,20 @@
 import { useMapEvents } from 'react-leaflet';
+import { useDispatch } from 'react-redux';
+import { openPopup, closePopup } from '../state/popupStateSlice';
 
 export const MapEvents = () => {
+  const dispatch = useDispatch();
   const map = useMapEvents({
     // change center coords when map is moved
     moveend: () => {
       localStorage.setItem('centerCoords', JSON.stringify(map.getCenter()));
       localStorage.setItem('zoom', JSON.stringify(map.getZoom()));
+    },
+    popupopen: () => {
+      dispatch(openPopup());
+    },
+    popupclose: () => {
+      dispatch(closePopup());
     },
   });
   return null;
